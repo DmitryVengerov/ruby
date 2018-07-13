48e8a0a7773c7fcd1fd6d7c05702ee51af3a4776
DmitriyVV <hardbeat34@gmail.com>
2018-07-13 16:45:27 +0300

    add 1

---
+++ les-6/numeric.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+require "active_support/all"
+
+=begin
+
+    Метод, возвращающий объект
+    класса ActiveSupport::Duration,
+    представляющий n часов, n минут, n
+    секунд
+
+=end
+
+p 1.month.ago
